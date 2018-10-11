7d4abaeed155954b19bfde25124a24d29f99b47a
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-10-11 12:53:49 +0000

    organized index

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // Here the web service should be setup and routes declared
-// TODO schema
-const express = require("express");
-const bodyParser = require("body-parser");
-
 const artistService = require("./services/artistService");
 const artService = require("./services/artService");
 const auctionService = require("./services/auctionService");
 const customerService = require("./services/customerService");
 
-const port = 5500;
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const port = 5500;
 const app = express();
 
 
