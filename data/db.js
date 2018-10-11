5b1b1fc5649bf40c3366ef7dd58a2f0fb016a511
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-10-11 00:22:17 +0000

    index og db

--- data/db.js
+++ data/db.js
@@ -6,7 +6,8 @@ const auctionBidSchema = require('../schemas/auctionBid');
 const customerSchema = require('../schemas/customer');
 const orderSchema = require('../schemas/order');
 
-const connection = mongoose.createConnection('insert-your-mongodb-connection-string-here', { useNewUrlParser: true });
+const connection = mongoose.createConnection('mongodb://veftdbuser:Abc12345>@ds125482.mlab.com:25482/mansion-de-subastas', { 
+    useNewUrlParser: true });
 
 module.exports = {
     Art: connection.model('Art', artSchema),
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Here the web service should be setup and routes declared
+// TODO schema
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const artistService = require("./services/artistService");
+const artService = require("./services/artService");
+const auctionService = require("./services/auctionService");
+const customerService = require("./services/customerService");
+
+const port = 5500;
+
+const app = express();
+
+
+
+
+
+
+
+// listens to port with factoral function
+app.listen(port, () => {
+  console.log("server is listening to port " + port);
+});
